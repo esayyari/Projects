0ae21e2bb635d05c7f93329480900029793fcc1d
mmalmir <mmalmir@ucsd.edu>
2014-01-15 22:01:29 -0800

    Update LogisticRegression.py
    first steps for SGD

--- 01/Code/LogisticRegression.py
+++ 01/Code/LogisticRegression.py
@@ -26,7 +26,29 @@ class LogisticRegression(Transformer):
     
     #the SGD training function
     def trainSGD(X,y):
-        
+        n,d = X.shape
+        #augment X with 1
+        augX = np.concatenate([X,np.ones([n,1])],axis=1)
+        #initialize parameters
+        beta = 0.01 * np.random.randn([d+1,1])
+        #learning rates, one for each parameter
+        landa = 0.9 * np.ones([d+1,1])
+        #shuffle data
+        idx = np.arange(n)
+        np.random.shuffle(idx)
+        #main loop
+        converged = False
+        cnt = 0
+        while not converged:
+            #pick the next shuffled sample
+            m = idx[cnt]
+            xm = X[m,:].reshape([1,-1])
+            #calculate Pm
+            Pm = 1. / (1. + np.exp( -np.dot(xm,beta) ) )
+            beta = beta + landa * (y[m]-Pm)*xm
+            #update landa
+            
+            cnt = cnt+1 % n
     # the LBFGS training function
     def trainLBFGS(X,y):
         
