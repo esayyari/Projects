48960170c43ac8f95916e33c044c5e55a5893c9d
mmalmir <mmalmir@ucsd.edu>
2014-01-18 13:22:29 -0800

    tests the LBFGS learning method
    This is a test run for LBFGS method. it trains an sklearn logistic regression on the same data for comparison.

---
+++ 01/Code/testLbfgs.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sklearn.linear_model as linear_model
+
+from LogisticRegression import LogisticRegression
+from readdata import read_data
+
+X_train,y_train = read_data("./Data/train")
+#X_train,y_train = X_train.toarray(),y_train.toarray()
+print type(X_train)
+print type(X_train)
+#convert labels to 0,1
+idx = np.where(y_train==-1)
+y_train[idx] = 0.
+lr = LogisticRegression(learning="LBFGS")
+lr.fit(X_train,y_train)
+X_test,y_test = read_data("./Data/test")
+idx = np.where(y_test==-1)
+y_test[idx] = 0.
+print "test score:",lr.score(X_test,y_test)
+
+sklr = linear_model.LogisticRegression(penalty="l2",C=10000.).fit(X_train,y_train)
+print "sklearn.lr score:",sklr.score(X_test,y_test)
