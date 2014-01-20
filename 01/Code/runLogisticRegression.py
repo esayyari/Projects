0b3c726d086815ade31c2ffc096414f8989d86e0
arya <airanmehr@gmail.com>
2014-01-20 00:27:00 -0800

    Best Performance bu Default Parameters

--- 01/Code/runLogisticRegression.py
+++ 01/Code/runLogisticRegression.py
@@ -3,20 +3,22 @@ import sklearn.linear_model as linear_model
 from LogisticRegression import LogisticRegression
 from readdata import read_data, LR_SGD_grid
 
-doGridSearch=True
+doGridSearch=False
 shuffle_examples, normalize_fetures= True , True
 X_train,y_train = read_data("./Data/train",normalize_fetures,shuffle_examples)
 X_test,y_test   = read_data("./Data/test",normalize_fetures)
 
 if doGridSearch:
     eta,edecay= LR_SGD_grid(X_train, y_train)
-    
-# lr = LogisticRegression(learning="LBFGS")
-lr = LogisticRegression(learning="SGD", eta_0=eta, eta_decay=edecay, max_epoch=5)
+else:
+    eta,edecay=0.01, 0.9
+solver="SGD"
+# lr = LogisticRegression(learning=solver)
+lr = LogisticRegression(learning=solver, eta_0=eta, eta_decay=edecay, max_epoch=2, batch_size=10)
 lr.fit(X_train,y_train)
 
 sklr = linear_model.LogisticRegression(penalty="l2",C=10000.).fit(X_train,y_train)
 
-print "test score:",lr.score(X_test,y_test)
-print "sklearn.lr score:",sklr.score(X_test,y_test)
+print solver, "Test Accuracy:",lr.score(X_test,y_test)
+print "SkitLearn Accuracy:",sklr.score(X_test,y_test)
 
